12343db24dd5175d305b759135af3323b8ea0f2c
pete <meltzerpete@gmail.com>
2020-10-26 09:17:29 +0000

    filter plot

--- analysis/figures_for_paper/ablation/instance_norm_plot.py
+++ analysis/figures_for_paper/ablation/instance_norm_plot.py
@@ -5,12 +5,16 @@ import numpy as np
 if __name__ == '__main__':
 
     dfs = {
-        'Gram': pd.read_csv('uvnet_solidmnist_all_raw_grams_layer_probe_scores_with_err.csv'),
-        'INorm Only': pd.read_csv('uvnet_solidmnist_all_inorm_layer_probe_scores_with_err.csv'),
-        'FNorm Only': pd.read_csv('uvnet_solidmnist_all_fnorm_layer_probe_scores_with_err.csv'),
+        'Gram': pd.read_csv('/home/pete/uvnet_solidmnist_all_raw_grams_layer_probe_scores_with_err.csv'),
+        'INorm Only': pd.read_csv('/home/pete/uvnet_solidmnist_all_inorm_layer_probe_scores_with_err.csv'),
+        'FNorm Only': pd.read_csv('/home/pete/uvnet_solidmnist_all_fnorm_layer_probe_scores_with_err.csv'),
     }
 
     for i, (version, df) in enumerate(dfs.items()):
+        if version == 'FNorm Only':
+            df = df.loc[0:3]
+        else:
+            df = df.loc[0:6]
         xticks = np.arange(len(df)) * (len(dfs) + 1)
         plt.bar(x=xticks + i,
                 height=df['linear_probe'],
@@ -27,7 +31,6 @@ if __name__ == '__main__':
         '4_fc',
         '5_GIN',
         '6_GIN',
-        'content'
     ]
     xticks = np.arange(len(labels)) * (len(dfs)+1) + 0.5
     ax.set_xticks(xticks + 1)
