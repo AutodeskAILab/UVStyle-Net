adf15aed18b6fd93cfb8792e3d23f56fa00f4927
pete <meltzerpete@gmail.com>
2020-10-27 20:12:40 +0000

    layer probes plot - make smaller

--- analysis/figures_for_paper/layer_probes/layer_probe_comparison_plot.py
+++ analysis/figures_for_paper/layer_probes/layer_probe_comparison_plot.py
@@ -1,35 +1,40 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    uvnet = pd.read_csv('uvnet_layer_probe_scores_with_err.csv')
-    pointnet = pd.read_csv('pointnet_layer_probe_scores_with_err.csv')
-    meshcnn = pd.read_csv('meshcnn_layer_probe_scores_with_err.csv')
 
-    uvnet['model'] = 'UV-Net'
-    pointnet['model'] = 'Pointnet++'
-    meshcnn['model'] = 'MeshCNN'
+    paths = {
+        'UV-Net': 'uvnet_layer_probe_scores_with_err.csv',
+        'Pointnet++': 'pointnet_layer_probe_scores_with_err.csv',
+        'MeshCNN': 'meshcnn_layer_probe_scores_with_err.csv',
+    }
 
-    df = pd.concat([uvnet, pointnet, meshcnn])
+    dfs = []
+    for model, path in paths.items():
+        df = pd.read_csv(path)
+        df['model'] = model
+        dfs.append(df)
 
-    fig, axes = plt.subplots(nrows=3)  # type: plt.Figure, np.ndarray
+    df = pd.concat(dfs).reset_index()
+
+    fig, ax = plt.subplots()  # type: plt.Figure, plt.Axes
 
     groups = df.groupby('model', sort=False)
-
     for i, (model, model_df) in enumerate(groups):
-        ax = axes[i]
-        ax.set_title(model)
-        ax.bar(np.arange(len(model_df)), model_df['linear_probe'],
-               yerr=model_df['linear_probe_err'])
-        # ax.errorbar(np.arange(len(model_df)), model_df['linear_probe'],
-        #             yerr=model_df['linear_probe_err'])
-        ax.set_xticks(np.arange(len(model_df)))
-        ax.set_yticks(np.arange(7) / 5)
-        ax.set_ylim([.6, 1])
-        # ax.plot([-.5, len(model_df) - .5], [.25, .25], '--', color='black')
-        ax.set_xticklabels(model_df['layer'], rotation='vertical')
-    fig.set_size_inches(6, 6)
+        ax.bar(x=model_df.index,
+               height=model_df['linear_probe'],
+               yerr=model_df['linear_probe_err'],
+               label=model)
+
+    ax.set_xticks(np.arange(len(df)))
+    ax.set_xticklabels(df['layer'], rotation='vertical')
+    ax.set_yticks(np.arange(7) / 5)
+    ax.set_ylim(.6, 1)
+    ax.legend(loc='lower right')
+    s = 0.9
+    fig.set_size_inches(6 * s, 3 * s)
     fig.tight_layout()
+
     fig.savefig('layer_probe_comparison.pdf')
     fig.show()
