3a26c3e6de3ac81c245a5e33cc35b1a8173175a9
pete <meltzerpete@gmail.com>
2020-11-03 12:53:20 +0000

    sub mu only font optimization with separate plots

--- analysis/figures_for_paper/user_refinement/font_selection_optimize_collate_and_plot_separate.py
+++ analysis/figures_for_paper/user_refinement/font_selection_optimize_collate_and_plot_separate.py
@@ -13,9 +13,9 @@ from util import Grams
 
 
 def add_heatmap(arr, ax, pos, neg, sigs1=None, sigs2=None):
-    im, cbar = heatmap(data=arr[list(reversed(np.arange(len(neg)))), :],
-                       row_labels=list(reversed(neg)),
-                       col_labels=pos,
+    im, cbar = heatmap(data=arr[list(reversed(np.arange(len(pos)))), :],
+                       row_labels=list(reversed(pos)),
+                       col_labels=neg,
                        ax=ax,
                        cmap="viridis",
                        label_top=False)
@@ -32,7 +32,7 @@ def add_heatmap(arr, ax, pos, neg, sigs1=None, sigs2=None):
     texts = texts.reshape(arr.shape)
 
     annotate_heatmap(im=im, valfmt="{x}",
-                     texts=texts[list(reversed(np.arange(len(neg)))), :],
+                     texts=texts[list(reversed(np.arange(len(pos)))), :],
                      textcolors=['white', 'black'],
                      threshold=arr.mean())
 
@@ -54,36 +54,34 @@ def collate_df(root_dir):
 
 def heatmap_and_std(ax, df, title, n_trials, pos, neg):
     # ax, err_ax = axes  # type: plt.Axes, plt.Axes
-    arr = np.zeros([len(neg), len(pos)])
-    err = np.zeros([len(neg), len(pos)])
-    gain = np.zeros([len(neg), len(pos)])
+    arr = np.zeros([len(pos), len(neg)])
+    err = np.zeros([len(pos), len(neg)])
+    gain = np.zeros([len(pos), len(neg)])
     sigs1 = np.zeros_like(arr)
     sigs2 = np.zeros_like(arr)
     for p_i, p, n_i, n in [(p_i, p, n_i, n) for (p_i, p) in enumerate(pos) for (n_i, n) in enumerate(neg)]:
         try:
-            arr[n_i, p_i] = df[df['pos_neg'] == str((p, n))]['score']
-            err[n_i, p_i] = df[df['pos_neg'] == str((p, n))]['err']
-            sigs1[n_i, p_i] = significance_test(mu_1=arr[0, 0],
+            arr[p_i, n_i] = df[df['pos_neg'] == str((p, n))]['score']
+            err[p_i, n_i] = df[df['pos_neg'] == str((p, n))]['err']
+            sigs1[p_i, n_i] = significance_test(mu_1=arr[0, 0],
                                                 std_1=err[0, 0],
-                                                mu_2=arr[n_i, p_i],
-                                                std_2=err[n_i, p_i],
+                                                mu_2=arr[p_i, n_i],
+                                                std_2=err[p_i, n_i],
                                                 n_trials=n_trials,
                                                 q=0.9)
-            sigs2[n_i, p_i] = significance_test(mu_1=arr[0, 0],
+            sigs2[p_i, n_i] = significance_test(mu_1=arr[0, 0],
                                                 std_1=err[0, 0],
-                                                mu_2=arr[n_i, p_i],
-                                                std_2=err[n_i, p_i],
+                                                mu_2=arr[p_i, n_i],
+                                                std_2=err[p_i, n_i],
                                                 n_trials=n_trials,
                                                 q=0.95)
-            gain[n_i, p_i] = arr[n_i, p_i] / arr[0, 0] if arr[0, 0] > 0. else 1.
+            gain[p_i, n_i] = arr[p_i, n_i] / arr[0, 0] if arr[0, 0] > 0. else 1.
         except:
             continue
     ax.set_title(title)
     add_heatmap(arr, ax, pos, neg, sigs1=sigs1, sigs2=sigs2)
-    ax.set_xlabel('No. of Positives')
-    ax.set_ylabel('No. of Negatives')
-    # err_ax.set_title(title + ' Increase')
-    # add_heatmap(gain, err_ax)
+    ax.set_xlabel('No. of Negatives')
+    ax.set_ylabel('No. of Positives')
     return arr, gain
 
 
@@ -95,14 +93,13 @@ def significance_test(mu_1, std_1, mu_2, std_2, n_trials, q=0.95):
     dist = z * std_diff
     return mu_2 - mu_1 > dist
 
+def set_size(fig: plt.Figure):
+    fig.set_size_inches(6, 4)
 
 if __name__ == '__main__':
     df = collate_df('results_solidmnist_all_sub_mu_only')
     df = df.groupby(['font', 'case', 'pos_neg']).mean().reset_index()
 
-    fig, axes = plt.subplots(nrows=len(df['font'].unique()) * 2,
-                             ncols=1,
-                             squeeze=True)  # type: plt.Figure, np.ndarray
     grams = Grams('../../uvnet_data/solidmnist_all_fnorm')
     fonts = {
         i: font_name for i, font_name in zip(grams.labels, map(lambda f: f[2:-10], grams.graph_files))
@@ -113,30 +110,47 @@ if __name__ == '__main__':
     arrs = []
     gains = []
     for i, ((font, case), font_df) in enumerate(df.groupby(['font', 'case'])):
-        arr, gain = heatmap_and_std(axes[i], font_df,
-                                    title=f'{font}: {fonts[font]}_{case} (20 Trials)',
+        fig, ax = plt.subplots()  # type: plt.Figure, plt.Axes
+        arr, gain = heatmap_and_std(ax, font_df,
+                                    title=f'{fonts[font]}_{case} (20 Trials)',
                                     n_trials=20,
                                     pos=pos,
                                     neg=neg)
+        set_size(fig)
+        fig.tight_layout()
+        fig.savefig(f'{fonts[font]}_{case} (20 Trials).pdf')
+        fig.show()
         arrs.append(arr)
         gains.append(gain)
-    s = .9
-    fig.set_size_inches(7 * s, 60 * s)
-    fig.tight_layout()
-    fig.savefig('svm_user_optimization_sub_mu_only_upper_lower.pdf')
-    # fig.show()
 
-    fig, axes = plt.subplots(ncols=2, squeeze=True)  # type: plt.Figure, np.ndarray
-    all_df = df.groupby('pos_neg').mean().reset_index()
+    fig, ax = plt.subplots()  # type: plt.Figure, plt.Axes
     num_fonts = len(df['font'].unique())
-    # heatmap_and_std(axes, all_df, f'Mean ({num_fonts} Fonts)', n_trials=num_fonts)
-    add_heatmap(arr=np.mean(np.stack(gains), axis=0),
-                ax=axes[0], pos=pos, neg=neg)
-    add_heatmap(arr=np.std(np.stack(gains), axis=0),
-                ax=axes[1], pos=pos, neg=neg)
-    s = .8
-    fig.set_size_inches(10 * s, 6 * s)
+    print(f'{num_fonts} fonts')
+
+    arr = np.mean(gains, axis=0)
+    err = np.std(gains, axis=0)
+    sigs1 = np.zeros_like(arr)
+    sigs2 = np.zeros_like(arr)
+
+    n_trials = 20 * num_fonts
+    for p_i, p, n_i, n in [(p_i, p, n_i, n) for (p_i, p) in enumerate(pos) for (n_i, n) in enumerate(neg)]:
+        sigs1[p_i, n_i] = significance_test(mu_1=arr[0, 0],
+                                            std_1=err[0, 0],
+                                            mu_2=arr[p_i, n_i],
+                                            std_2=err[p_i, n_i],
+                                            n_trials=n_trials,
+                                            q=0.9)
+        sigs2[p_i, n_i] = significance_test(mu_1=arr[0, 0],
+                                            std_1=err[0, 0],
+                                            mu_2=arr[p_i, n_i],
+                                            std_2=err[p_i, n_i],
+                                            n_trials=n_trials,
+                                            q=0.95)
+    add_heatmap(arr, ax, pos, neg, sigs1, sigs2)
+    ax.set_title(f'Mean Gain ({num_fonts} Fonts)')
+    ax.set_xlabel('No. of Negatives')
+    ax.set_ylabel('No. of Positives')
+    set_size(fig)
     fig.tight_layout()
-    fig.savefig('svm_user_optimization_sub_mu_only_all_upper_lower.pdf')
+    fig.savefig(f'Mean Gain ({num_fonts} Fonts).pdf')
     fig.show()
-    # fig.show()
