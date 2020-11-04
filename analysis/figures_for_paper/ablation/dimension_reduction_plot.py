0909b514036a7448af1106a45b19ef71f5196288
pete <meltzerpete@gmail.com>
2020-11-04 14:08:25 +0000

    remove content + set proper x scale

--- analysis/figures_for_paper/ablation/dimension_reduction_plot.py
+++ analysis/figures_for_paper/ablation/dimension_reduction_plot.py
@@ -2,37 +2,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-if __name__ == '__main__':
-    df = pd.read_csv('dimension_reduction_probe_results.csv')
-    df['dims'] = df['dims'].apply(lambda d: str(int(d)) if d > 0 else 'Original')
 
-    layer_names = [
-        '0_feats (21)',
-        '1_conv (2,080)',
-        '2_conv (8,256)',
-        '3_conv (32,896)',
-        '4_fc (3,2896)',
-        '5_GIN (2,080)',
-        '6_GIN (2,080)',
-        'UV-Net Embedding (136)'
-    ]
+def load_df(d):
+    df = pd.read_csv(f'dim_reduction_results/dimension_reduction_probe_results_{d}.csv', sep=',')
+    df['dims'] = [21, 2_080, 8_256, 32_896, 32_896, 2_080, 2_080, 136] if d is None else d
+    return df
+
+if __name__ == '__main__':
+    df = pd.concat([load_df(d) for d in [None, 70, 25, 10, 3]])
+    layer_names = {
+        '0_feats': '0_feats (21)',
+        '1_conv1': '1_conv (2,080)',
+        '2_conv2': '2_conv (8,256)',
+        '3_conv3': '3_conv (32,896)',
+        '4_fc': '4_fc (3,2896)',
+        '5_GIN_1': '5_GIN (2,080)',
+        '6_GIN_2': '6_GIN (2,080)',
+        'content': 'UV-Net Embedding\n(136)'
+    }
 
     fig, ax = plt.subplots()  # type: plt.Figure, plt.Axes
     xticks = np.arange(df['dims'].nunique())
     for i, (layer, group_df) in enumerate(df.groupby(['layer'])):
-        ax.plot(xticks,
-                group_df['score'],
+        if layer == '0_feats':
+            group_df = group_df.reset_index().drop([1, 2])
+        elif layer == 'content':
+            continue
+        ax.plot(group_df['dims'],
+                group_df['linear_probe'],
                 label=layer_names[layer])
-        ax.fill_between(xticks,
-                        group_df['score'] - group_df['err'],
-                        group_df['score'] + group_df['err'],
-                        alpha=0.1)
+        ax.fill_between(group_df['dims'],
+                        group_df['linear_probe'] - group_df['linear_probe_err'],
+                        group_df['linear_probe'] + group_df['linear_probe_err'],
+                        alpha=0.2)
 
     ax.legend()
     ax.set_xlabel('Reduced Dimensions')
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(['None', '70', '25', '10', '3'])
-    ax.set_ylabel('Linear Probe Score')
+    lim = ax.get_xlim()
+    # ax.loglog()
+    ax.semilogx()
+
+    ax.plot([3, 32_896 * 1.2], [1/378, 1/378], '--', color='black')
     fig.set_size_inches(6, 3)
     fig.tight_layout()
     fig.savefig('dimension_reduction_plot.pdf')
