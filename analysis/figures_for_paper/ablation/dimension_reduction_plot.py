beaf2e630d0a07d1a00a33fccaa0abd60fb88d70
pete <meltzerpete@gmail.com>
2020-11-05 11:04:19 +0000

    tweaking plot

--- analysis/figures_for_paper/ablation/dimension_reduction_plot.py
+++ analysis/figures_for_paper/ablation/dimension_reduction_plot.py
@@ -8,6 +8,7 @@ def load_df(d):
     df['dims'] = [21, 2_080, 8_256, 32_896, 32_896, 2_080, 2_080, 136] if d is None else d
     return df
 
+
 if __name__ == '__main__':
     df = pd.concat([load_df(d) for d in [None, 70, 25, 10, 3]])
     layer_names = {
@@ -28,21 +29,25 @@ if __name__ == '__main__':
             group_df = group_df.reset_index().drop([1, 2])
         elif layer == 'content':
             continue
-        ax.plot(group_df['dims'],
-                group_df['linear_probe'],
-                label=layer_names[layer])
+        lines = ax.plot(group_df['dims'],
+                        group_df['linear_probe'],
+                        label=layer_names[layer])
+        ax.plot(group_df['dims'].iloc[0],
+                group_df['linear_probe'].iloc[0],
+                'o',
+                color=lines[-1].get_color())
         ax.fill_between(group_df['dims'],
                         group_df['linear_probe'] - group_df['linear_probe_err'],
                         group_df['linear_probe'] + group_df['linear_probe_err'],
                         alpha=0.2)
 
-    ax.legend()
+    ax.legend(fontsize='small')
     ax.set_xlabel('Reduced Dimensions')
     lim = ax.get_xlim()
     # ax.loglog()
     ax.semilogx()
 
-    ax.plot([3, 32_896 * 1.2], [1/378, 1/378], '--', color='black')
+    ax.plot([3, 32_896], [1 / 378, 1 / 378], '--', color='black')
     fig.set_size_inches(6, 3)
     fig.tight_layout()
     fig.savefig('dimension_reduction_plot.pdf')
