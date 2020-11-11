205201983d764aaee34e8563694b675b6e660775
pete <meltzerpete@gmail.com>
2020-11-11 16:57:20 +0000

    clean up invalid solids

--- analysis/abc_all/clean_solids.py
+++ analysis/abc_all/clean_solids.py
@@ -2,13 +2,13 @@ import os
 import sys
 
 if __name__ == '__main__':
-    abc_dir = '/home/pete/brep_style/abc/bin'
+    abc_dir = '/home/pete/brep_style/abc/pngs'
     invalid_solids = 'invalid_solids.txt'
 
     with open(invalid_solids, 'r') as file:
         for name in file.readlines():
             try:
-                solid = f'{abc_dir}/{name[:-1]}.bin'
+                solid = f'{abc_dir}/{name[:-1]}.png'
                 print('removing:', solid)
                 os.remove(solid)
             except Exception as e:
