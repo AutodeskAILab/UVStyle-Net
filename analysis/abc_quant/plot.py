84cec0a8cab9e32fadd6194d562aa97222ee758a
pete <meltzerpete@gmail.com>
2021-03-16 13:29:13 +0000

    final changes for logreg crossval + thumbnails for abc classes

--- analysis/abc_quant/plot.py
+++ analysis/abc_quant/plot.py
@@ -15,7 +15,7 @@ def to_model(data_root):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('results_log_reg_cross_val_sklearn_balanced.csv', sep=';')
+    df = pd.read_csv('results_log_reg_cross_val_sklearn_balanced_f1_weighted.csv', sep=';')
     config_df = pd.DataFrame(df['config'].apply(literal_eval).values.tolist())
     df = pd.concat([df.drop('config', axis=1), config_df], axis=1)
     df['cats'] = df['cats_dirs'].apply(to_cats)
@@ -24,16 +24,16 @@ if __name__ == '__main__':
     psnet = df.loc[df['model'] == 'psnet']
 
     df2 = uvnet.join(psnet.set_index('cats'), on='cats', how='left', lsuffix='_uvnet', rsuffix='_psnet')
-    result = df2[['cats', 'val_acc_uvnet', 'val_std_uvnet', 'val_acc_psnet', 'val_std_psnet']]
-    result['diff'] = result['val_acc_uvnet'] - result['val_acc_psnet']
+    result = df2[['cats', 'mean_f1_uvnet', 'std_f1_uvnet', 'mean_f1_psnet', 'std_f1_psnet']]
+    result['diff'] = result['mean_f1_uvnet'] - result['mean_f1_psnet']
     result.sort_values('diff', ascending=False, inplace=True)
 
-    print(result[['cats', 'val_acc_uvnet', 'val_std_uvnet', 'val_acc_psnet', 'val_std_psnet','diff']].to_markdown())
+    print(result[['cats', 'mean_f1_uvnet', 'std_f1_uvnet', 'mean_f1_psnet', 'std_f1_psnet','diff']].to_markdown())
 
-    result['UVStyle-Net'] = result[['val_acc_uvnet', 'val_std_uvnet']].apply(lambda x: f'${x[0]:.3f} \pm {x[1]:.3f}$', axis=1)
-    result['PSNet'] = result[['val_acc_psnet', 'val_std_psnet']].apply(lambda x: f'${x[0]:.3f} \pm {x[1]:.3f}$', axis=1)
+    result['UVStyle-Net'] = result[['mean_f1_uvnet', 'std_f1_uvnet']].apply(lambda x: f'${x[0]:.3f} \pm {x[1]:.3f}$', axis=1)
+    result['PSNet*'] = result[['mean_f1_psnet', 'std_f1_psnet']].apply(lambda x: f'${x[0]:.3f} \pm {x[1]:.3f}$', axis=1)
 
     result['UVStyle-Net'].loc[result['diff'] > 0] = result['UVStyle-Net'].loc[result['diff'] > 0].apply(lambda x: '$\mathbf{' + x[1:-1] + '}$')
-    result['PSNet'].loc[result['diff'] < 0] = result['PSNet'].loc[result['diff'] < 0].apply(lambda x: '$\mathbf{' + x[1:-1] + '}$')
+    result['PSNet*'].loc[result['diff'] < 0] = result['PSNet*'].loc[result['diff'] < 0].apply(lambda x: '$\mathbf{' + x[1:-1] + '}$')
 
-    # print(result[['cats','UVStyle-Net','PSNet','diff']].to_latex(escape=False))
+    print(result[['cats','UVStyle-Net','PSNet*','diff']].to_latex(escape=False))
